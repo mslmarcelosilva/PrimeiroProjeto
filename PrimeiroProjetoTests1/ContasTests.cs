b3a63cd67d22d46bd90b6f2e5f3479c562d7c3f7
Marcelo <mslmarcelosilva@gmail.com>
2021-12-13 15:32:02 -0300

    Aprendendo a trabalhar com testes

---
+++ PrimeiroProjetoTests1/ContasTests.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PrimeiroProjeto;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrimeiroProjeto.Tests
+{
+    [TestClass()]
+    public class ContasTests
+    {
+        [TestMethod()]
+        public void SomaTest() {
+            var a = 5;
+            var b = 5;
+            var esperado = 10;
+            var conta = new Contas();
+
+            var result = conta.Soma(a, b);
+
+            Assert.AreEqual(esperado, result);
+        }
+        [TestMethod()]
+        public void SubtracaoTest() {
+            var a = 5;
+            var b = 5;
+            var esperado = 0;
+            var conta = new Contas();
+
+            var result = conta.Subtracao(a, b);
+
+            Assert.AreEqual(esperado, result);
+        }
+        [TestMethod()]
+        public void MultiplicacaoTest() {
+            var a = 5;
+            var b = 5;
+            var esperado = 25;
+            var conta = new Contas();
+
+            var result = conta.Multiplicacao(a, b);
+
+            Assert.AreEqual(esperado, result);
+        }
+        [TestMethod()]
+        public void DivisaoTest() {
+            var a = 5;
+            var b = 5;
+            var esperado = 1;
+            var conta = new Contas();
+
+            var result = conta.Divisao(a, b);
+
+            Assert.AreEqual(esperado, result);
+        }
+    }
+}
